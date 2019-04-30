7173341a1821360ecb55afe475c8b6fe45f91cad
Alexander Gorin <plattapb@gmail.com>
2019-04-30 22:36:23 +0300

    Updated fav system

--- app/src/main/java/com/example/recyclerviewtask/DataOperator.kt
+++ app/src/main/java/com/example/recyclerviewtask/DataOperator.kt
@@ -1,10 +1,14 @@
 package com.example.recyclerviewtask
 
+import com.example.recyclerviewtask.DataOperator.allItemsList
+import com.example.recyclerviewtask.DataOperator.favouritesList
+import com.example.recyclerviewtask.DataOperator.weatherWithSectionsList
+
 /**
  * Object that does all operations with data.
  * Has recycler's view adapter to notify about changes.
  * Has [weatherWithSectionsList] that contains all data, including sections.
- * Has [favouritesList] and [notFavouritesList] for better sorting system.
+ * Has [favouritesList] and [allItemsList] for better sorting system.
  *
  * @author Alexander Gorin
  */
@@ -13,7 +17,7 @@ object DataOperator {
     lateinit var recyclerViewAdapter: WeatherForecastFragment.WeatherForecastRecyclerViewAdapter
     val weatherWithSectionsList = arrayListOf<ItemWeatherForecast>()
     private val favouritesList = arrayListOf<WeatherForecast>()
-    private val notFavouritesList = arrayListOf<WeatherForecast>()
+    private val allItemsList = arrayListOf<WeatherForecast>()
     private val indent: Int
         get() = favouritesList.size + sectionsList.size
 
@@ -22,11 +26,10 @@ object DataOperator {
             add(0, sectionsList[0])
             add(1, sectionsList[1])
             val favList = weatherForecastList.filter { it.isFavourite }
-            val notFavList = weatherForecastList.filter { !it.isFavourite }
             favouritesList.addAll(favList)
-            notFavouritesList.addAll(notFavList)
+            allItemsList.addAll(weatherForecastList)
             addAll(1, favouritesList)
-            addAll(indent, notFavouritesList)
+            addAll(indent, allItemsList)
         }
     }
 
@@ -36,52 +39,54 @@ object DataOperator {
             add(0, sectionsList[0])
             add(1, sectionsList[1])
             addAll(1, favouritesList)
-            addAll(indent, notFavouritesList)
+            addAll(indent, allItemsList)
             recyclerViewAdapter.notifyDataSetChanged()
         }
     }
 
     fun addToFavourites(position: Int) {
-        val item = (weatherWithSectionsList.removeAt(position) as WeatherForecast)
-        notFavouritesList.remove(item)
+        val item = (weatherWithSectionsList[position] as WeatherForecast)
+        val allItemsIndex = allItemsList.indexOf(item)
+        allItemsList[allItemsIndex].isFavourite = true
         favouritesList.add(0, item.apply {
             isFavourite = true
         })
         weatherWithSectionsList.add(1, item)
-        recyclerViewAdapter.notifyItemMoved(position, 1)
+        recyclerViewAdapter.notifyItemInserted(1)
     }
 
     fun removeFromFavourites(position: Int) {
-        val item = (weatherWithSectionsList.removeAt(position) as WeatherForecast)
-        favouritesList.remove(item)
-        notFavouritesList.add(0, item.apply {
-            isFavourite = false
-        })
-        weatherWithSectionsList.add(indent, item)
-        recyclerViewAdapter.notifyItemMoved(position, indent)
+        val item = (weatherWithSectionsList[position] as WeatherForecast)
+        val favIndex = favouritesList.indexOf(item)
+        favouritesList.removeAt(favIndex)
+        val allItemsIndex = allItemsList.indexOf(item)
+        allItemsList[allItemsIndex].isFavourite = false
+        item.isFavourite = false
+        weatherWithSectionsList.removeAt(favIndex + 1)
+        recyclerViewAdapter.notifyItemRemoved(favIndex + 1)
     }
 
     fun sortNameAsc() {
         favouritesList.sortBy { it.cityName }
-        notFavouritesList.sortBy { it.cityName }
+        allItemsList.sortBy { it.cityName }
         addAllItemsToList()
     }
 
     fun sortNameDesc() {
         favouritesList.sortByDescending { it.cityName }
-        notFavouritesList.sortByDescending { it.cityName }
+        allItemsList.sortByDescending { it.cityName }
         addAllItemsToList()
     }
 
     fun sortTempAsc() {
         favouritesList.sortBy { it.weatherTemp }
-        notFavouritesList.sortBy { it.weatherTemp }
+        allItemsList.sortBy { it.weatherTemp }
         addAllItemsToList()
     }
 
     fun sortTempDesc() {
         favouritesList.sortByDescending { it.weatherTemp }
-        notFavouritesList.sortByDescending { it.weatherTemp }
+        allItemsList.sortByDescending { it.weatherTemp }
         addAllItemsToList()
     }
 }
